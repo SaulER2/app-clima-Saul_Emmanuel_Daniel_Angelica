c59271050fb58471ab8f0ebffc552daa86467224
Daniel Maria <daniel.jair.maria@gmail.com>
2025-12-01 01:22:28 -0500

    se termino la vistal mobil

--- frontend/src/components/PopularCities.jsx
+++ frontend/src/components/PopularCities.jsx
@@ -6,22 +6,26 @@ export default function PopularCities({ popularCities, selectedIndex }) {
 
 
     return (
-
-        <section className='popular-cities mobile-only' id='popular-cities'>
-            {popularCities.map((city, index) => {
-                const dayForecast = city.forecast[selectedIndex];
-                return (
-                    <div className='popular-card' key={index}>
-                        <div className='popular-card-left'>
-                            <p className='popular-temp'>{dayForecast.temp}°</p>
-                            <p className='city-name'>{city.name}</p>
+        <section>
+            <article>
+                <p className='localidades'>Localidades Populares</p>
+            </article>
+            <article className='popular-cities mobile-only' id='popular-cities'>
+                {popularCities.map((city, index) => {
+                    const dayForecast = city.forecast[selectedIndex];
+                    return (
+                        <div className='popular-card' key={index}>
+                            <div className='popular-card-left'>
+                                <p className='popular-temp'>{dayForecast.temp}°</p>
+                                <p className='city-name'>{city.name}</p>
+                            </div>
+                            <div className='popular-card-right'>
+                                <p className='city-icon'>{dayForecast.icon}</p>
+                            </div>
                         </div>
-                        <div className='popular-card-right'>
-                            <p className='city-icon'>{dayForecast.icon}</p>
-                        </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </article>
         </section>
     );
 }
